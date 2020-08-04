4c4ef2e5800e3a23427af05963243cc222f39508
Kilinadan <Safeer.Kilinadan@bakerhughes.com>
2020-08-04 18:38:21 +0530

    Dropdown widget completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
 import Search from './Search';
+import Dropdown from './Dropdown';
 
 
 const items = [
@@ -16,12 +17,36 @@ const items = [
         title:'How do you use React?',
         content:'You use react by creating components'
     }
+];
+
+const options = [
+    {
+        label:'The Color Red',
+        value:'red'
+    },
+    {
+        label:'The Color Green',
+        value:'Green'
+    },
+    {
+        label:'A shade of blue',
+        value:'blue'
+    }
 ]
+
 export default () => {
+
+    const [selected,setSelected] = useState(options[0]);
+
     return(
         <div>
             {/* <Accordion items={items}/> */}
-            <Search/>
+            {/* <Search/> */}
+            <Dropdown 
+                selected={selected}
+                options={options}
+                onSelectedChange={setSelected}
+            />
         </div>
     )
 };
